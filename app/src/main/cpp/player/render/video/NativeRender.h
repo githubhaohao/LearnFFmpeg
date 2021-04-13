b833e25ed52a1b986707f9111f86ce546f5e9089
字节流动 <haohaochang86@gmail.com>
2021-04-13 22:13:46 +0800

    字节流动

--- app/src/main/cpp/player/render/video/NativeRender.h
+++ app/src/main/cpp/player/render/video/NativeRender.h
@@ -1,6 +1,10 @@
-//
-// Created by 字节流动 on 2020/6/19.
-//
+/**
+ *
+ * Created by 公众号：字节流动 on 2021/3/16.
+ * https://github.com/githubhaohao/LearnFFmpeg
+ * 最新文章首发于公众号：字节流动，有疑问或者技术交流可以添加微信 Byte-Flow ,领取视频教程, 拉你进技术交流群
+ *
+ * */
 
 #ifndef LEARNFFMPEG_NATIVERENDER_H
 #define LEARNFFMPEG_NATIVERENDER_H
--- app/src/main/java/com/byteflow/learnffmpeg/camera/Camera2Wrapper.java
+++ app/src/main/java/com/byteflow/learnffmpeg/camera/Camera2Wrapper.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * Created by 公众号：字节流动 on 2021/3/16.
+ * https://github.com/githubhaohao/LearnFFmpeg
+ * 最新文章首发于公众号：字节流动，有疑问或者技术交流可以添加微信 Byte-Flow ,领取视频教程, 拉你进技术交流群
+ *
+ * */
+
 package com.byteflow.learnffmpeg.camera;
 
 import android.Manifest;
