ba76d44d7f8404ea81944964b93400f20ac4d9e6
字节流动 <haohaochang86@gmail.com>
2021-10-11 09:53:49 +0800

    update

--- app/src/main/cpp/common/ThreadSafeQueue.h
+++ app/src/main/cpp/common/ThreadSafeQueue.h
@@ -1,6 +1,10 @@
-//
-// Created by 公众号：字节流动 on 2021/2/22.
-//
+/**
+ *
+ * Created by 公众号：字节流动 on 2021/3/16.
+ * https://github.com/githubhaohao/LearnFFmpeg
+ * 最新文章首发于公众号：字节流动，有疑问或者技术交流可以添加微信 Byte-Flow ,领取视频教程, 拉你进技术交流群
+ *
+ * */
 
 #ifndef LEARNFFMPEG_THREADSAFEQUEUE_H
 #define LEARNFFMPEG_THREADSAFEQUEUE_H
